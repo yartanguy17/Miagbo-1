450f6382e0bd5190f69244ffa1b4f1fe2ec0c4c3
Lord Seventh <kevintiassou@gmail.com>
2020-10-27 14:26:29 +0000

    First edit: fetching data in some components

---
+++ components/partials/homepage/default/HomeBanner.vue
@@ -0,0 +1,101 @@
+<template lang="html">
+    <div class="ps-home-banner ps-home-banner--1" v-if="!$fetchState.pending">
+        <div class="ps-container">
+            <div class="ps-section__left">
+                <div class="ps-carousel" v-swiper:mySwiper="swiperOption">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="ps-banner">
+                                <nuxt-link to="/shop">
+                                    <a>
+                                        <img
+                                            :src="baseUrl + products[0].images"
+                                            alt="martfury"
+                                        />
+                                    </a>
+                                </nuxt-link>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="ps-banner">
+                                <nuxt-link to="/shop">
+                                    <a>
+                                        <img
+                                            :src="baseUrl + products[1].images"
+                                            alt="martfury"
+                                        />
+                                    </a>
+                                </nuxt-link>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="ps-banner">
+                                <nuxt-link to="/shop">
+                                    <a>
+                                        <img
+                                            :src="baseUrl + products[2].images"
+                                            alt="martfury"
+                                        />
+                                    </a>
+                                </nuxt-link>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Carousel controls-->
+                    <div class="swiper-nav">
+                        <span class="swiper-arrow swiper-prev"
+                            ><i class="icon-chevron-left"></i
+                        ></span>
+                        <div class="swiper-arrow swiper-next">
+                            <i class="icon-chevron-right"></i>
+                        </div>
+                    </div>
+                    <div
+                        class="swiper-pagination swiper-pagination-bullets"
+                    ></div>
+                </div>
+            </div>
+            <div class="ps-section__right">
+                <nuxt-link to="/shop" class="ps-collection">
+                    <img
+                        :src="baseUrl + products[3].images"
+                        alt="martfury"
+                    />
+                </nuxt-link>
+                <nuxt-link to="/shop" class="ps-collection">
+                    <img
+                        :src="baseUrl + products[4].images"
+                        alt="martfury"
+                    />
+                </nuxt-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeBanner',
+    data() {
+        return {
+            swiperOption: {
+                loop: true,
+                slidesPerView: 1,
+                spaceBetween: 1,
+                navigation: {
+                    nextEl: '.swiper-next',
+                    prevEl: '.swiper-prev'
+                }
+            },
+            baseUrl : 'http://51.89.97.33:5500',
+            products: [],
+        };
+    }, 
+    async fetch(){
+        this.products = await fetch('http://51.89.97.33:5500/api/products')
+            .then(res => res.json());
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/partials/homepage/default/HomeDefaultTopCategories.vue
@@ -0,0 +1,124 @@
+<template lang="html">
+    <div class="ps-top-categories" v-if="!$fetchState.pending">
+        <div class="ps-container">
+            <h3>{{ $t('autopart.topCategories') }}</h3>
+            <div class="row">
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>                        
+                        <img
+                            :src="baseUrl + categories[0].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[0].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[1].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[1].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[2].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[2].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[3].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[3].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[4].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[4].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[5].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[5].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[6].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[6].nom}}</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div class="ps-block--category">
+                        <nuxt-link to="/shop">
+                            <a class="ps-block__overlay"></a>
+                        </nuxt-link>
+                        <img
+                            :src="baseUrl + categories[7].img"
+                            alt="martfury"
+                        />
+                        <p>{{categories[7].nom}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeDefaultTopCategories',
+    data(){
+        return{
+            baseUrl: 'http://51.89.97.33:5500',
+            categories: [],
+        }
+    },
+    async fetch(){
+        this.categories = await fetch('http://51.89.97.33:5500/api/categories')
+            .then(res => res.json());
+    }
+};
+
+</script>
+
+<style lang="scss" scoped></style>
